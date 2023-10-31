bdc384608e6590bcd14a3372abbc7b4a73b9d02d
Hana_182 <108173681+hanamahes78@users.noreply.github.com>
2023-10-31 23:37:51 +0700

    Add files via upload

---
+++ pendaftaran-siswa/config.php
@@ -0,0 +1,28 @@
+<?php
+
+$server = "127.0.0.1:3307";
+$user = "root";
+$password = "";
+$nama_database = "pendaftaran_siswa";
+
+$db = mysqli_connect($server, $user, $password, $nama_database);
+
+if (!$db) {
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+
+// $host = "127.0.0.1:3307";
+// $user = "root";
+// $pass = "";
+// $database = "pendaftaran_siswa";
+
+// $koneksi = mysqli_connect($host, $user, $pass);
+// if ($koneksi) {
+//     $buka = mysqli_select_db($koneksi, $database);
+//     echo "Database dapat terhubung";
+//     if (!$buka) {
+//         echo "Database tidak dapat terhubung";
+//     }
+// } else {
+//     echo "MySQL tidak terhubung";
+// }
